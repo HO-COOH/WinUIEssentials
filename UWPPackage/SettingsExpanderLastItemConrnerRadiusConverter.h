8accf417d25adeb0bf47ef349d08b2b49b610513
Peter <szpeter@Yeah.net>
2024-10-01 02:02:30 +0800

    Feat: Add CornerRadiusConverter for rounded SettingsExpander bottom corners, add mica to both project, close #5

---
+++ UWPPackage/SettingsExpanderLastItemConrnerRadiusConverter.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "SettingsExpanderLastItemConrnerRadiusConverter.g.h"
+
+namespace winrt::UWPPackage::implementation
+{
+    struct SettingsExpanderLastItemConrnerRadiusConverter : SettingsExpanderLastItemConrnerRadiusConverterT<SettingsExpanderLastItemConrnerRadiusConverter>
+    {
+        SettingsExpanderLastItemConrnerRadiusConverter() = default;
+
+        winrt::Windows::Foundation::IInspectable Convert(
+            winrt::Windows::Foundation::IInspectable const& value,
+            winrt::Windows::UI::Xaml::Interop::TypeName const& targetType,
+            winrt::Windows::Foundation::IInspectable const& parameter,
+            winrt::hstring const& language
+        );
+
+        winrt::Windows::Foundation::IInspectable ConvertBack(
+            winrt::Windows::Foundation::IInspectable const& value,
+            winrt::Windows::UI::Xaml::Interop::TypeName const& targetType,
+            winrt::Windows::Foundation::IInspectable const& parameter,
+            winrt::hstring const& language
+        );
+    };
+}
+
+namespace winrt::UWPPackage::factory_implementation
+{
+    struct SettingsExpanderLastItemConrnerRadiusConverter : SettingsExpanderLastItemConrnerRadiusConverterT<SettingsExpanderLastItemConrnerRadiusConverter, implementation::SettingsExpanderLastItemConrnerRadiusConverter>
+    {
+    };
+}
---
+++ WinUI3Package/SettingsExpanderLastItemConrnerRadiusConverter.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "SettingsExpanderLastItemConrnerRadiusConverter.h"
+#if __has_include("SettingsExpanderLastItemConrnerRadiusConverter.g.cpp")
+#include "SettingsExpanderLastItemConrnerRadiusConverter.g.cpp"
+#endif
+
+namespace winrt::WinUI3Package::implementation
+{
+	winrt::Windows::Foundation::IInspectable SettingsExpanderLastItemConrnerRadiusConverter::Convert(
+		winrt::Windows::Foundation::IInspectable const& value,
+		winrt::Windows::UI::Xaml::Interop::TypeName const&,
+		winrt::Windows::Foundation::IInspectable const&,
+		winrt::hstring const&)
+	{
+		if (auto cornerRadius = value.try_as<winrt::Microsoft::UI::Xaml::CornerRadius>())
+		{
+			return winrt::box_value(winrt::Microsoft::UI::Xaml::CornerRadius{
+				.TopLeft = 0,
+				.TopRight = 0,
+				.BottomRight = cornerRadius->BottomRight,
+				.BottomLeft = cornerRadius->BottomLeft
+			});
+		}
+		return value;
+	}
+
+	winrt::Windows::Foundation::IInspectable SettingsExpanderLastItemConrnerRadiusConverter::ConvertBack(
+		winrt::Windows::Foundation::IInspectable const& value,
+		winrt::Windows::UI::Xaml::Interop::TypeName const&,
+		winrt::Windows::Foundation::IInspectable const&,
+		winrt::hstring const&)
+	{
+		return value;
+	}
+}
