f9f03522339d5dfb6aed87a2f4fc98006e491cd9
Peter <szpeter@Yeah.net>
2025-09-16 21:18:05 +0800

    Add element theme tooltip

---
+++ WinUI3Example/ElementThemeToStringConverter.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "ElementThemeToStringConverter.h"
+#if __has_include("ElementThemeToStringConverter.g.cpp")
+#include "ElementThemeToStringConverter.g.cpp"
+#endif
+
+namespace winrt::WinUI3Example::implementation
+{
+	winrt::Windows::Foundation::IInspectable ElementThemeToStringConverter::Convert(
+		winrt::Windows::Foundation::IInspectable const& value,
+		winrt::Windows::UI::Xaml::Interop::TypeName const&,
+		winrt::Windows::Foundation::IInspectable const&,
+		winrt::hstring const&)
+	{
+		switch (winrt::unbox_value<winrt::Microsoft::UI::Xaml::ElementTheme>(value))
+		{
+			case winrt::Microsoft::UI::Xaml::ElementTheme::Default:
+				return winrt::box_value(L"Default");
+			case winrt::Microsoft::UI::Xaml::ElementTheme::Light:
+				return winrt::box_value(L"Light");
+			case winrt::Microsoft::UI::Xaml::ElementTheme::Dark:
+				return winrt::box_value(L"Dark");
+		}
+	}
+
+	winrt::Windows::Foundation::IInspectable ElementThemeToStringConverter::ConvertBack(
+		winrt::Windows::Foundation::IInspectable const& value,
+		winrt::Windows::UI::Xaml::Interop::TypeName const&,
+		winrt::Windows::Foundation::IInspectable const&,
+		winrt::hstring const&)
+	{
+		throw winrt::hresult_not_implemented{};
+	}
+}
---
+++ WinUI3Example/ElementThemeToStringConverter.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "ElementThemeToStringConverter.g.h"
+
+namespace winrt::WinUI3Example::implementation
+{
+    struct ElementThemeToStringConverter : ElementThemeToStringConverterT<ElementThemeToStringConverter>
+    {
+        ElementThemeToStringConverter() = default;
+
+		winrt::Windows::Foundation::IInspectable Convert(
+            winrt::Windows::Foundation::IInspectable const& value, 
+            winrt::Windows::UI::Xaml::Interop::TypeName const& targetType, 
+            winrt::Windows::Foundation::IInspectable const& parameter, 
+            winrt::hstring const& language);
+
+		winrt::Windows::Foundation::IInspectable ConvertBack(
+			winrt::Windows::Foundation::IInspectable const& value,
+			winrt::Windows::UI::Xaml::Interop::TypeName const& targetType,
+			winrt::Windows::Foundation::IInspectable const& parameter,
+			winrt::hstring const& language);
+
+    };
+}
+
+namespace winrt::WinUI3Example::factory_implementation
+{
+    struct ElementThemeToStringConverter : ElementThemeToStringConverterT<ElementThemeToStringConverter, implementation::ElementThemeToStringConverter>
+    {
+    };
+}
