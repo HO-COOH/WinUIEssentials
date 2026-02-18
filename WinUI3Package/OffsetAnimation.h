838286fc3b1104563615c234a2f6df6e65c974a0
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-02-18 21:45:20 +0800

    Add Implicit Animations.

---
+++ WinUI3Package/OffsetAnimation.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "UI/Xaml/Media/Animations/OffsetAnimation.g.h"
+
+namespace winrt::WinUI3Package::UI::Xaml::Media::Animations::implementation
+{
+	struct OffsetAnimation : OffsetAnimationT<OffsetAnimation>
+	{
+		OffsetAnimation();
+
+		Microsoft::UI::Xaml::Media::Animation::EasingMode EasingMode() const;
+		void EasingMode(Microsoft::UI::Xaml::Media::Animation::EasingMode const& value);
+
+		Windows::Foundation::Numerics::float3 From() const;
+		void From(Windows::Foundation::Numerics::float3 const& value);
+
+		Windows::Foundation::Numerics::float3 To() const;
+		void To(Windows::Foundation::Numerics::float3 const& value);
+
+    private:
+		Microsoft::UI::Xaml::Media::Animation::EasingMode m_easingMode{};
+		Windows::Foundation::Numerics::float3 m_from{};
+		Windows::Foundation::Numerics::float3 m_to{};
+	};
+}
+
+namespace winrt::WinUI3Package::UI::Xaml::Media::Animations::factory_implementation
+{
+	struct OffsetAnimation : winrt::WinUI3Package::UI::Xaml::Media::Animations::factory_implementation::OffsetAnimationT<OffsetAnimation, implementation::OffsetAnimation>
+	{
+	};
+}
---
+++ WinUI3Package/OpacityAnimation.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include "OpacityAnimation.h"
+#if __has_include("UI/Xaml/Media/Animations/OpacityAnimation.g.cpp")
+#include "UI/Xaml/Media/Animations/OpacityAnimation.g.cpp"
+#endif
+
+using namespace winrt;
+
+namespace winrt::WinUI3Package::UI::Xaml::Media::Animations::implementation
+{
+	OpacityAnimation::OpacityAnimation()
+	{
+	}
+
+	Microsoft::UI::Xaml::Media::Animation::EasingMode OpacityAnimation::EasingMode() const
+	{
+		return m_easingMode;
+	}
+
+	void OpacityAnimation::EasingMode(Microsoft::UI::Xaml::Media::Animation::EasingMode const& value)
+	{
+		m_easingMode = value;
+	}
+
+	double OpacityAnimation::From() const
+	{
+		return m_from;
+	}
+
+	void OpacityAnimation::From(double value)
+	{
+		m_from = value;
+	}
+
+	double OpacityAnimation::To() const
+	{
+		return m_to;
+	}
+
+	void OpacityAnimation::To(double value)
+	{
+		m_to = value;
+	}
+}
