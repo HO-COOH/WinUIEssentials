d40c015ac70587c05b62e179e83f42f3de4e4a07
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-04-19 17:46:36 +0800

    Fix include path

--- WinUI3Example/WindowedContentDialogPage.xaml.cpp
+++ WinUI3Example/WindowedContentDialogPage.xaml.cpp
@@ -5,7 +5,7 @@
 #endif
 #include "App.xaml.h"
 
-#include <include/ScopedButtonDisabler.hpp>
+#include <ScopedButtonDisabler.hpp>
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
